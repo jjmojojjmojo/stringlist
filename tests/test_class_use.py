709f2b027b569b9fe5c1c6c4dd2fb59ae58eca4c
Josh Johnson <lionface.lemonface@gmail.com>
2013-10-23 20:40:06 -0400

    Added test for edge case

--- tests/test_class_use.py
+++ tests/test_class_use.py
@@ -28,3 +28,26 @@ class TestDescripterClassUse(unittest.TestCase):
         obj.roles += 'hola'
         
         self.assertEquals(obj.roles, ['great', 'scott', 'hola'])
+        
+    def test_set_many_roles(self):
+        obj = self._class()
+        
+        obj.roles = 'first', 'second'
+        
+        self.assertEquals(obj.roles, ['first', 'second'])
+        
+    def test_initialize_many_roles(self):
+        """
+        Can we initialize the StringList in a class with multiple values?
+        """
+        from stringlist import StringList
+        
+        class SpecialThing(object):
+            roles = StringList()
+            
+            def __init__(self, roles):
+                self.roles = roles
+        
+        obj = SpecialThing(['one', 'two'])
+        
+        self.assertEquals(obj.roles, ['one', 'two'])
